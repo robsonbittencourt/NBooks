2f97f8da17c4ee6090344f81c63b5c6c203e6742
Robson <robson.luizv@gmail.com>
2013-09-06 16:17:06 -0300

    Adicionando models para usuario, emprestimo e reserva

---
+++ src/model/Reserva.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Reserva {
+	
+	private Usuario usuario;
+	private Livro livro;
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	public Livro getLivro() {
+		return livro;
+	}
+	
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
+	
+}
