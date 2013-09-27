ef77c70847eb914baabcf74fef0390fa9ada6658
Pablo Ruan <ruanmyv@gmail.com>
2013-09-26 23:13:28 -0300

    Ajustes nas rotas e nos dados para popular

--- backlog/server.js
+++ backlog/server.js
@@ -1,7 +1,5 @@
 var express = require('express'),
-    users = require('./routes/users'),
-    drinks = require('./routes/drinks'),
-    checkins = require('./routes/checkins');
+    books = require('./routes/books');
  
 var app = express();
 
@@ -9,56 +7,16 @@ app.use(express.bodyParser());
 
 // default route
 app.get('/', function(req, res){
-	res.redirect('/users/');
+	//res.redirect('/users/');
 });
 
-// users
-app.get('/login', users.login);
+// books
+app.get('/books', books.findAll);
+//app.post('/books/', books.create);
+app.get('/books/:id', books.findById);
+//app.put('/books/:id', books.update);
+//app.delete('/books/:id', books.delete);
 
-app.get('/users', users.findAll);
-app.post('/users', users.createNewUser);
-app.get('/drinks', drinks.findAll);
-
-app.get('/users/:id', users.findById);
-app.get('/users/:id/friends', users.findFriends);
-//app.get('/users/:id/dashboard', users.findDashboard);
-
-//app.put('/users/:id/', users.updateUser);
-
-// drinks
-
-app.get('/drinks/:id', drinks.findById);
-
-// checkins
-app.post('/checkins', checkins.createNewCheckin);
-
-/*
-
-GET /users/
-GET /users/:id
-GET /users/:id/friends
-GET /users/:id/Dashboard
-POST /users/
-{
-	"name": "name",
-	"email": "email",
-	"password" : "password"
-}
-PUT /users:id
-
-GET /drinks/
-GET /drinks/[?name]&[type]
-GET /drinks/:id
-
-POST /checkins/
-{
-	"userId" : "userId",
-	"drinkId" : "drinkId",
-	"comment" : "comment",
-	"private" : "private"
-}
-
-*/
 
 app.listen(3000);
 console.log('Listening on port 3000...');
